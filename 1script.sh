3f82d54b282628db97c8881580de3599cd85af3b
grinchikav <grinchikav@gmail.com>
2021-07-26 17:25:56 +0300

    first commit

--- 1script.sh
+++ 1script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grep -rl 'nginx-01.com' /home/grin/myprojekt/one/* | tail -100 | xargs -i@ sed -i 's/nginx-01.com/nginx-02.com/g' @
+grep -rl 'nginx-01.com' ./one/* | tail -100 | xargs -i@ sed -i 's/nginx-01.com/nginx-02.com/g' @
--- 3script.sh
+++ 3script.sh
@@ -2,7 +2,7 @@
 hs=$(hostname)
 cores=$(nproc)
 ds=$(df -h --output=size --total | awk 'END {print $1}')
-echo "Hostname: $hs" > /home/grin/myprojekt/task3
-echo "CPU cores: $cores" >> /home/grin/myprojekt/task3
-grep MemTotal /proc/meminfo >> /home/grin/myprojekt/task3
-echo "Disk space: $ds" >> /home/grin/myprojekt/task3
+echo "Hostname: $hs" > ./task3
+echo "CPU cores: $cores" >> ./task3
+grep MemTotal /proc/meminfo >> ./task3
+echo "Disk space: $ds" >> ./task3
